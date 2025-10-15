349eaa194e5412e70a95b00fb53450c5a11f2c04
renan-borsato <reb840@ensign.edu>
2025-10-15 00:16:27 -0600

    Fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
+      <body>
         <Navbar />
         <main className="max-w-6xl mx-auto px-6 py-12">{children}</main>
         <Footer />
