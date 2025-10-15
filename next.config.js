3dc11a85969ea2d27feb52d0b5aa1ad4808c87a0
renan-borsato <reb840@ensign.edu>
2025-10-14 23:16:38 -0600

    Fix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
-// get env variable
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const env = process.env.NODE_ENV || 'development';
 
 /** @type {import('next').NextConfig} */
 const nextConfigProd = {
-  output: 'export',
-  basePath: '',
+  output: 'export', // static export
   images: {
     loader: 'custom',
     loaderFile: './src/lib/image.loader.js',
